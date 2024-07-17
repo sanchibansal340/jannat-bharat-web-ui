019ff789daea4eedd3c9e723073bf72dc2b1bb88
Sanchi Bansal <sanchibansal340@gmail.com>
2024-07-18 00:41:35 +0530

    Sanchi | Update. responsive carousel banner, add prettier config

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,29 +1,78 @@
-import React from 'react';
-import { Navbar } from '../../components/utils';
-import { Grid, Container, Box } from '@mui/material';
-import welcomePageImg from '../../assets/banner.jpg';
+import React, { useState, useEffect } from 'react'
+import { Navbar } from '../../components/utils'
+import { Grid, Typography, Box } from '@mui/material'
+import HomepageCarouselImg1 from '../../assets/homepage-carousel-img1.jpg'
+import HomepageCarouselImg2 from '../../assets/homepage-carousel-img2.jpg'
+import HomepageCarouselImg3 from '../../assets/homepage-carousel-img3.jpg'
+import HomepageCarouselImg4 from '../../assets/homepage-carousel-img4.jpg'
 import './HomePage.css'
 
-export const HomePage = () => {
+const homepageCarouselImages = [
+    HomepageCarouselImg1,
+    HomepageCarouselImg2,
+    HomepageCarouselImg3,
+    HomepageCarouselImg4,
+]
+
+const styles = {
+    bannerImage: {
+        position: 'relative',
+        width: '100%',
+        height: 'auto',
+        backgroundImage: `url("https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/10/landing-background-top.jpg")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        py: '3rem',
+    },
+}
+
+const HomePage = () => {
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(
+                (prevIndex) => (prevIndex + 1) % homepageCarouselImages.length
+            )
+        }, 2000)
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
-        <Grid container xs={12}>
+        <Box>
             <Navbar />
-            <Grid item xs={12}>
-                <Box style={{ position: 'relative' }}
-                >
-                    <img
-                        style={{ height: 'auto', width: '100%' }}
-                        src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/10/landing-background-top.jpg"
-                        alt='img'
-                        // src={welcomePageImg}
-                    />
-                    <div className='welcome-banner-overlay'>
-                        <h1 className='welcome-banner-heading'>
-                            Travel and Explore the rich Culture of India with us
-                        </h1>
-                    </div>
-                </Box>
-            </Grid>
-        </Grid>
+            <Box sx={styles.bannerImage}>
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} md={7}>
+                        <Typography
+                            variant="h4"
+                            component="div"
+                            className="welcome-banner-heading"
+                        >
+                            Explore the rich Culture of India with <br />
+                            <b> JANNAT BHARAT </b>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <img
+                            src={homepageCarouselImages[currentIndex]}
+                            alt="Foreground"
+                            style={{
+                                maxWidth: '20rem',
+                                height: '30rem',
+                                border: '2rem solid white',
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
     )
 }
+
+export default HomePage
--- src/pages/utils.js
+++ src/pages/utils.js
@@ -1,1 +1,1 @@
-export {HomePage} from './HomePage/HomePage';
+export { default as HomePage } from './HomePage/HomePage'
