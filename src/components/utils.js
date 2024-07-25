cb9a99f197676a9c96c02bdfa40468268b18b255
Sanchi Bansal <sanchibansal340@gmail.com>
2024-07-26 00:19:19 +0530

    Sanchi | Add. 2nd option of banner

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,6 @@
 export { default as Navbar } from './Navbar/Navbar'
-export { default as Banner } from './Banner/Banner'
+// export { default as Banner } from './Banner/Banner'
+export { default as Banner } from './Banner/Banner2'
 export { default as About } from './About/About'
 export { default as UpcomingTrips } from './UpcomingTrips/UpcomingTrips'
 export { default as Reviews } from './Carousel/Reviews'
