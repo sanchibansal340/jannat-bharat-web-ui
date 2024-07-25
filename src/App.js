e87751a3103ee61a2212a5902f866430b4a8f810
Sanchi Bansal <sanchibansal340@gmail.com>
2024-07-25 05:54:25 +0530

    Add. reviews, footer, upcoming trips section in homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import './App.css'
+import { HomePage } from './pages/utils'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TourDetailsContainer from './containers/TourDetailsContainer';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1d4d54',
+        },
+        secondary: {
+            main: '#3B90A0',
+        },
+        background: {
+            main: '#F8F4F1',
+        },
+    },
+    typography: {
+        h1: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+        h2: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+        h3: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+        h4: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+        h5: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+        h6: {
+            fontFamily: 'Satisfy, cursive',
+            wordSpacing: '0.2em',
+        },
+    },
+})
 
 function App() {
   return (
