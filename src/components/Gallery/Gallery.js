03d7f9b8b47c8a4156abe450b3e19b570ffbfc9a
Sanchi Bansal <sanchibansal340@gmail.com>
2024-08-12 00:46:33 +0530

    delete. static gallery image list

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -7,38 +7,6 @@ import { fetchData } from '../../services/HomePageService'
 import './Gallery.css'
 import ShowMoreButtonComponent from '../ShowMoreButtonComponent'
 
-// const images = [
-//     {
-//         src: 'https://images.unsplash.com/photo-1505069446780-4ef442b5207f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D',
-//         width: 1,
-//         height: 2,
-//     },
-//     {
-//         src: 'https://images.unsplash.com/photo-1477064996809-dae46985eee7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fHw%3D',
-//         width: 2,
-//         height: 2,
-//     },
-//     {
-//         src: 'https://images.unsplash.com/photo-1609137144813-7d9921338f24?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D',
-//         width: 1,
-//         height: 1,
-//     },
-//     {
-//         src: 'https://images.unsplash.com/photo-1478860002487-680cc42afbeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTR8fHxlbnwwfHx8fHw%3D',
-//         width: 2,
-//         height: 1,
-//     },
-//     {
-//         src: 'https://images.unsplash.com/photo-1534313314376-a72289b6181e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTh8fHxlbnwwfHx8fHw%3D',
-//         width: 1,
-//         height: 2,
-//     },
-//     {
-//         src: 'https://images.unsplash.com/photo-1541410965313-d53b3c16ef17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjB8fHxlbnwwfHx8fHw%3D',
-//         width: 1,
-//         height: 1,
-//     },
-// ]
 
 const Gallery = () => {
     const settings = {
